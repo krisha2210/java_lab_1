917bcafb91ddc64b0b82ffab15d1c8f52f2ce010
krisha2210 <krisha.shah.btech2023@sitpune.edu.in>
2025-01-20 20:46:18 +0530

    Delete cal directory

--- cal/main.java
+++
@@ -1,9 +0,0 @@
-class Main{
-
-
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println("Addition: " + calculator.addition());
-        System.out.println("Subtraction: " + calculator.subtraction());
-    }
-}
